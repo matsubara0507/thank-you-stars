5434cb4405a41a24dc9e6014816a6e8ec204d5d8
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-11-30 00:54:43 +0900

    Refactor: rm ok package

--- lib/mix/tasks/thank_you_stars.ex
+++ lib/mix/tasks/thank_you_stars.ex
@@ -1,21 +1,19 @@
 defmodule Mix.Tasks.ThankYouStars do
   use Mix.Task
-  require OK
 
   @shortdoc "thank you stars !"
 
   def run(_args) do
     Mix.Task.run("app.start")
 
-    OK.try do
-      token <- ThankYouStars.load_token()
-    after
-      client = Tentacat.Client.new(%{access_token: token})
-
-      ThankYouStars.load_deps_packages()
-      |> Enum.map(&(ThankYouStars.star_package(&1, client) |> IO.puts()))
-    rescue
-      emessage -> IO.puts(emessage)
+    case ThankYouStars.load_token() do
+      {:ok, token} -> thank_you_stars(Tentacat.Client.new(%{access_token: token}))
+      {:error, emessage} -> IO.puts(emessage)
     end
+  end
+
+  defp thank_you_stars(client) do
+    ThankYouStars.load_deps_packages()
+    |> Enum.map(&(ThankYouStars.star_package(&1, client) |> IO.puts()))
   end
 end
